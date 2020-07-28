02e8f3d19585973f00302a403b319ca39c3dc3da
unknown <catalin.binzari99@gmail.com>
2020-07-28 07:31:04 +0300

    99 header links

--- client/src/components/Header_footer/Header/index.js
+++ client/src/components/Header_footer/Header/index.js
@@ -1,7 +1,70 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux';
 class Header extends Component {
+
+    state = {
+        page: [
+            {
+                name: 'Home',
+                linkTo: '/',
+                public: true //public
+            },
+            {
+                name: 'Home',
+                linkTo: '/shop',
+                public: true
+            }
+        ],
+        user: [
+            {
+                name: 'My Cart',
+                linkTo: '/user/cart',
+                public: false  //not public
+            },
+            {
+                name: 'My Account',
+                linkTo: '/user/dashboard',
+                public: false
+            },
+            {
+                name: 'Log in',
+                linkTo: '/register_login',
+                public: true
+            },
+            {
+                name: 'Log out',
+                linkTo: '/user/logout',
+                public: false
+            }
+        ]
+    }
+    defaultLink = (item, i) => (
+        <Link to={item.linkTo} key={i}>
+            {item.name}
+        </Link>
+    )
+    showLinks = (type) => {
+        let list = [];
+        if (this.props.user.userData) { //check if the user is logged in
+            type.forEach((item) => {
+                if (!this.props.user.userData.isAuth) { //if user is not auth
+                    if (item.public === true) {
+                        list.push(item)
+                    }
+                } else {
+                    if (item.name !== "Log in") {
+                        list.push(item)
+                    }
+                }
+            });
+        }
+        return list.map((item, i) => {
+            return this.defaultLink(item, i)
+        })
+    }
     render() {
+
         return (
             <header className="bck_b_light">
                 <div className="container">
@@ -12,10 +75,10 @@ class Header extends Component {
                     </div>
                     <div className="right">
                         <div className="top">
-                            LINKS
+                            {this.showLinks(this.state.user)}
                         </div>
                         <div className="bottom">
-                            LINKS
+                            {this.showLinks(this.state.page)}
                         </div>
                     </div>
                 </div>
@@ -23,5 +86,10 @@ class Header extends Component {
         );
     }
 }
-
-export default Header;
+//connect to redux
+function mapStateToProps(state) { //to inject the props we get from redux to connect()
+    return {
+        user: state.user
+    }
+}
+export default connect(mapStateToProps)(Header); 
--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -3,57 +3,58 @@ import { connect } from 'react-redux';
 import { auth } from '../actions/user_actions';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-export default function(ComposedClass,reload,adminRoute = null){
+export default function (ComposedClass, reload, adminRoute = null) {  //function that returns a function form a class
     class AuthenticationCheck extends Component {
 
         state = {
             loading: true
         }
 
-        componentDidMount(){
-            this.props.dispatch(auth()).then(response =>{
+        componentDidMount() {
+            this.props.dispatch(auth()).then(response => {
                 let user = this.props.user.userData;
                 console.log(user);
 
-                if(!user.isAuth){
-                    if(reload){
+                if (!user.isAuth) {
+                    if (reload) {
                         this.props.history.push('/register_login')
                     }
-                } else{
-                    if(adminRoute && !user.isAdmin){
+                } else {
+                    if (adminRoute && !user.isAdmin) {
                         this.props.history.push('/user/dashboard')
-                    } else{
-                        if(reload === false){
+                    } else {
+                        if (reload === false) {
                             this.props.history.push('/user/dashboard')
                         }
                     }
                 }
-                this.setState({loading:false})
+                this.setState({ loading: false })
             })
         }
 
 
         render() {
-            if(this.state.loading){
+            if (this.state.loading) {
                 return (
                     <div className="main_loader">
-                        <CircularProgress style={{color:'#2196F3'}} thickness={7}/> 
+                        <CircularProgress style={{ color: '#2196F3' }} thickness={7} />
                     </div>
                 )
             }
             return (
-               <ComposedClass {...this.props} user={this.props.user}/>
+                <ComposedClass {...this.props} user={this.props.user} />
             );
         }
     }
 
-    function mapStateToProps(state){
+    function mapStateToProps(state) { //for redux
         return {
             user: state.user
         }
     }
 
     return connect(mapStateToProps)(AuthenticationCheck)
+    //if we are using connect and are using mapStateToProps, we can 
 }
 
 
