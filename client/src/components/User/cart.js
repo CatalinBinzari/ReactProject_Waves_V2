010aaea1f7e03835b184a2f609be6aae5e0ce0df
unknown <catalin.binzari99@gmail.com>
2020-08-06 19:56:26 +0300

    /admin/manage_categories, displayed all categories in a nice way

--- client/src/components/User/cart.js
+++ client/src/components/User/cart.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import UserLayout from '../../hoc/user';
+import UserProductBlock from '../utils/User/product_block'
 import { connect } from 'react-redux';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome'
 import faFrown from '@fortawesome/fontawesome-free-solid/faFrown'
 import faSmile from '@fortawesome/fontawesome-free-solid/faSmile'
-import { getCartItems } from '../../actions/user_actions'
+import { getCartItems } from '../../actions/user_actions';
 class UserCart extends Component {
 
     state = {
@@ -14,27 +15,38 @@ class UserCart extends Component {
         showSuccess: false
     }
     componentDidMount() {
-        let cartItem = [];
+        let cartItems = [];
         let user = this.props.user;
         if (user.userData.cart) {
             if (user.userData.cart.length > 0) {
                 user.userData.cart.forEach(item => {
-                    cartItem.push(item.id) //push just id's from cart to cartItem
+                    cartItems.push(item.id) //push just id's from cart to cartItem
                 });
 
-                this.props.dispatch(getCartItems(cartItem, user.userData.cart))
-                // .then(() => {
+                this.props.dispatch(getCartItems(cartItems, user.userData.cart))
+                    .then(() => {
 
-                // })
+                    })
 
             }
         }
     }
+    removeFromCart() {
+
+    }
+
     render() {
         return (
             <UserLayout>
                 <div>
-                    cart
+                    <h1>My carts</h1>
+                    <div className="user_cart">
+                        <UserProductBlock
+                            products={this.props.user}
+                            type="cart"
+                            removeItem={(id) => this.removeFromCart(id)}
+                        />
+                    </div>
                 </div>
             </UserLayout>
         );
--- client/src/reducers/user_reducer.js
+++ client/src/reducers/user_reducer.js
@@ -26,7 +26,7 @@ export default function (state = {}, action) {
                 }
             }
         case GET_CART_ITEMS_USER:
-            return { ...state, cardDetail: action.payload }
+            return { ...state, cartDetail: action.payload }
         default:
             return state;
     }
